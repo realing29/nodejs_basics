0ee48ad693312e020bdfdbeee434594f9c8dd299
realing29 <realing29@gmail.com>
2020-08-17 15:13:30 +0500

    Start

---
+++ index.js
@@ -0,0 +1,7 @@
+// (function (require, module, exports, __filename, __dirname, ) {
+const obj = require('./user')
+
+console.log(obj.user)
+
+obj.sayHello();
+// })()
---
+++ user.js
@@ -0,0 +1,17 @@
+const user = {
+  name: 'Elena',
+  age: 25
+}
+
+const user2 = {
+  name: 'Ivan',
+  age: 42
+}
+
+module.exports = {
+  user: user,
+  sayHello() {
+    console.log('Hello');
+
+  }
+}
