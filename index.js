59b8fefb8d670be9001ae5e26c4eb3fc4aa9a81b
realing29 <realing29@gmail.com>
2020-09-06 07:04:54 +0500

    http

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-// (function (require, module, exports, __filename, __dirname, ) {
-const obj = require('./user')
+const http = require('http')
 
-console.log(obj.user)
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.write('<h1>Hello from nodejs</h1>')
+  res.write('<h2>Hello from nodejs</h2>')
+  res.write('<h3>Hello from nodejs</h3>')
 
-obj.sayHello();
-// })()
+  res.end(`
+    <div style="background: red; width: 200px; height: 200px;">
+      <span>text</span>
+    </div>
+  `)
+})
+
+server.listen('3000', () => {
+  console.log('Server is running')
+})
