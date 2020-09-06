9c408f521e6a9a40751ea19d4f587dc87a5fc8b6
realing29 <realing29@gmail.com>
2020-09-06 07:19:37 +0500

    nodemon

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.write('<h1>Hello from nodejs</h1>')
-  res.write('<h2>Hello from nodejs</h2>')
-  res.write('<h3>Hello from nodejs</h3>')
 
-  res.end(`
-    <div style="background: red; width: 200px; height: 200px;">
-      <span>text</span>
-    </div>
-  `)
 })
 
 server.listen('3000', () => {
